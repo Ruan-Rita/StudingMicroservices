0b3a53dec6b2afaf2048267e27fe02e420f198b2
Ruan-Rita <ruan@cambioreal.com>
2023-07-06 23:58:25 -0300

    add some layouts for front end

---
+++ FirstProject_Posts/client/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="h-screen">
+      <div className="flex h-full bg-red-100 content-center justify-center flex-1">
+        <div className="w-[1000px] mt-8">
+          <h1 className="font-bold text-2xl text-gray-700">Blog Application</h1>
+          <div className="rounded-md mt-4 p-5 bg-white h-[400px]">
+            Content
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
