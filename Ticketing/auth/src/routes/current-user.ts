1ba1bbff4f94e8621df182adae0adc7d9c17c96e
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2023-11-21 23:59:59 -0300

    get current user

--- Ticketing/auth/src/routes/current-user.ts
+++ Ticketing/auth/src/routes/current-user.ts
@@ -1,15 +1,21 @@
 import express, { Request, Response } from "express";
+import jwt from 'jsonwebtoken'
 
 const router = express.Router();
 router.get(`/api/user/currentuser`, (req: Request, res: Response) => {
-    res.json({
-        data: {
-            name: {
-                first: "ruan",
-                last: "rita"
-            }
-        }
-    })
+    if (req.session?.jwt) {
+
+        return res.json({ currentUser: {} })
+    }
+
+    try {
+        const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!)
+        res.json({ currentUser: payload })
+
+    } catch (error) {
+        return res.json({ currentUser: {} })
+    }
+
 })
 
 export { router as currentUserRoutes }
