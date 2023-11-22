1cdc3f6dccdbd03c51f4293220a394287f188a90
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2023-11-22 00:08:47 -0300

    remove bad code

--- Ticketing/auth/src/routes/current-user.ts
+++ Ticketing/auth/src/routes/current-user.ts
@@ -4,18 +4,16 @@ import jwt from 'jsonwebtoken'
 const router = express.Router();
 router.get(`/api/user/currentuser`, (req: Request, res: Response) => {
     if (!req.session?.jwt) {
-        return res.json({ currentUser: "sem token" })
+        return res.json({ currentUser: null })
     }
 
     try {
         const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!)
         res.json({ currentUser: payload })
-
     } catch (error) {
-        return res.json({ currentUser: 'token errado' })
+        return res.json({})
     }
-
-    res.json({ m: 'aconteceu nada' })
+    res.json({})
 })
 
 export { router as currentUserRoutes }
