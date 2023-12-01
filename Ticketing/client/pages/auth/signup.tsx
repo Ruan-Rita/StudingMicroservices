c1ff1fea5ddbaf01a99f1e6f6523394235698d62
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2023-11-30 23:31:35 -0300

    re-settings client

---
+++ Ticketing/client/pages/auth/signup.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+
+    return (
+        <form>
+            <input className="bg-gray-100 block p-2 mb-1 select-none outline-none" placeholder="nome" type="text" name="" id="" />
+            <input className="bg-gray-100 block p-2 mb-1 select-none outline-none" placeholder="senha" type="text" name="" id="" />
+        </form>
+    )
+} 
